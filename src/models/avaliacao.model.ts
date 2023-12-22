5c1e835837dc31fb1766b2811663d5f221af3d6c
Diego <dl7designer@gmail.com>
2023-12-21 23:03:34 -0300

    codigo comentado sem uso deletado

--- src/models/avaliacao.model.ts
+++ src/models/avaliacao.model.ts
@@ -2,20 +2,15 @@ import { v4 } from "uuid";
 import { Aluno } from "./aluno.model";
 
 export class Avaliacao {
-    // private _id: string;
     public typeAluno: Aluno
 
     constructor(public idAluno: string, private _disciplina: string, private _nota: number, typeAluno: Aluno) {
         this.typeAluno = typeAluno
     }
 
-    // public get id() {
-    //     return this._id;
-    // }
-
     public toJson() {
         return {
-            // id: this._id,
+
             idAluno: this.idAluno,
             nota: this._nota,
             disciplina: this._disciplina,
