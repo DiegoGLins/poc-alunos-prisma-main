0e660cb9f810d4104691334bd62b3f0111ae87cd
Diego <dl7designer@gmail.com>
2024-01-07 19:11:29 -0300

    add doc interno tsdoc metodo login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,6 +3,17 @@ import repository from "../database/prisma.connection";
 import jwt from 'jsonwebtoken'
 
 class AuthService {
+    /**
+      * Metódo responsável por logar o usuário no sistema.
+      *
+      * @param email - O primeiro parâmetro a ser passado é o email do usuário
+      * @param password - O segundo parâmetro a ser passado é a senha do usuário
+      * @returns - Se email e senha do usuário forem válidos, retorna os dados
+      * do usuário com as propriedades: id, email, nome e o token gerado com jwt
+      * ao efetuar o login
+      *
+      */
+
     public async login(email: string, password: string): Promise<Result> {
         const result = await repository.aluno.findUnique({
             where: {
